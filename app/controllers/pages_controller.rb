c6c8af253641b88d19e0a80a1d1d5067cc375fa0
Joe Guerra <guerra_joe@hotmail.com>
2020-03-23 15:54:24 -0700

    working on navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def about
      add_breadcrumb 'MASSDUMP', :root_path
-     add_breadcrumb 'about', :pages_about_path
+     add_breadcrumb 'About', :pages_about_path
   end
 
   def activity
@@ -9,32 +9,32 @@ class PagesController < ApplicationController
 
   def contact
      add_breadcrumb 'MASSDUMP', :root_path
-     add_breadcrumb 'contact', :pages_contact_path
+     add_breadcrumb 'Contact', :pages_contact_path
   end
 
   def cookies
     add_breadcrumb 'MASSDUMP', :root_path
-    add_breadcrumb 'cookies', :pages_cookies_path
+    add_breadcrumb 'Cookies', :pages_cookies_path
   end
 
   def dashboard
      add_breadcrumb 'MASSDUMP', :root_path
-     add_breadcrumb 'dashboard', :pages_dashboard_path
+     add_breadcrumb 'Dashboard', :pages_dashboard_path
   end
 
   def error
     add_breadcrumb 'MASSDUMP', :root_path
-    add_breadcrumb 'error', :pages_error_path
+    add_breadcrumb 'Error', :pages_error_path
   end
 
   def faq
      add_breadcrumb 'MASSDUMP', :root_path
-    add_breadcrumb 'faq', :pages_faq_path
+    add_breadcrumb 'FAQ', :pages_faq_path
   end
 
   def index
      add_breadcrumb 'MASSDUMP', :root_path
-     add_breadcrumb 'home', :pages_index_path
+     add_breadcrumb 'Home', :pages_index_path
   end
 
   def invitations
@@ -42,22 +42,22 @@ class PagesController < ApplicationController
 
   def invite
      add_breadcrumb 'MASSDUMP', :root_path
-     add_breadcrumb 'invite', :pages_invite_path
+     add_breadcrumb 'Invite', :pages_invite_path
   end
 
   def jobs
     add_breadcrumb 'MASSDUMP', :root_path
-    add_breadcrumb "jobs", :pages_jobs_path
+    add_breadcrumb "Jobs", :pages_jobs_path
   end
 
   def privacy
     add_breadcrumb 'MASSDUMP', :root_path
-    add_breadcrumb 'privacy', :pages_privacy_path
+    add_breadcrumb 'Privacy', :pages_privacy_path
   end
 
   def random
     add_breadcrumb 'MASSDUMP', :root_path
-    add_breadcrumb 'random', :pages_random_path
+    add_breadcrumb 'Random', :pages_random_path
 
     @random = Product.published.limit(12).order("RANDOM()")
 
@@ -65,7 +65,7 @@ class PagesController < ApplicationController
 
  def terms
     add_breadcrumb 'MASSDUMP', :root_path
-    add_breadcrumb 'terms', :pages_terms_path
+    add_breadcrumb 'Terms', :pages_terms_path
   end
 
 end
