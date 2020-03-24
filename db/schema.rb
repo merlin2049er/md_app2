b25283ce206c5a0ec5c47f9e8f77cdafe41a1009
Joe Guerra <guerra_joe@hotmail.com>
2020-03-24 14:55:07 -0700

    add category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_225439) do
+ActiveRecord::Schema.define(version: 2020_03_24_214850) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "picurl"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
