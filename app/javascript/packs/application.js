dba72bba4f6fb0bec8a0417a4460651208ff3965
Joe Guerra <guerra_joe@hotmail.com>
2020-03-31 16:57:57 -0700

    ..

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("cocoon")
+
+
 
 require("packs/bootstrap.js")
 require("packs/custom.js")
