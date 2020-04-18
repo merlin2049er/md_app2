753e4690c4b8a2e51eff6509cad7b4a72f6d6a46
Joe Guerra <guerra_joe@hotmail.com>
2020-04-18 11:22:33 -0700

    ..switch to mailgun

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,11 +2,11 @@
 
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.sendgrid.net',
+  address: 'smtp.mailgun.org',
   port: '587',
   authentication: :plain,
-  user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
+  user_name: ENV['MAILGUN_SMTP_LOGIN'],
+  password: ENV['MAILGUN_SMTP_PASSWORD'],
   domain: 'heroku.com',
   enable_starttls_auto: true
 }
