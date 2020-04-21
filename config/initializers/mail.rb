deb15590b40027d0f2775d40e499e860fd91b516
Joe Guerra <guerra_joe@hotmail.com>
2020-04-21 06:13:20 -0700

    fixed sendgrid with api key

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,11 +2,11 @@
 
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.mailgun.org',
+  address: 'smtp.sendgrid.net',
   port: '587',
   authentication: :plain,
-  user_name: ENV['MAILGUN_SMTP_LOGIN'],
-  password: ENV['MAILGUN_SMTP_PASSWORD'],
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
   domain: 'heroku.com',
   enable_starttls_auto: true
 }
