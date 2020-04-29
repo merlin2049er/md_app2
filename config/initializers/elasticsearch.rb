1aeba4d69550c42da16d9f8e4e923ab8658475d7
saqibgujjar <39758201+saqibgujjar@users.noreply.github.com>
2020-04-29 20:55:54 +0500

    update

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,5 +1,5 @@
 config = {
-  host: "https://paas:f49902c6629cd3282b0d3b86c1610c65@oin-us-east-1.searchly.com/",
+  host: "https://paas:f49902c6629cd3282b0d3b86c1610c65@oin-us-east-1.searchly.com",
   transport_options: {
     request: { timeout: 5 }
   }
@@ -10,4 +10,4 @@ if File.exists?("config/elasticsearch.yml")
 end
 
 
-Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV['ELASTICSEARCH_URL'] || "localhost:9200"
+Elasticsearch::Model.client = Elasticsearch::Client.new(config)
