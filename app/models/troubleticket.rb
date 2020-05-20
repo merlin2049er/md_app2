96db0799635309490918ee7b47cf576129a171bb
Joe Guerra <guerra_joe@hotmail.com>
2020-05-19 17:25:33 -0700

    finally fixed the status issue

--- app/models/troubleticket.rb
+++ app/models/troubleticket.rb
@@ -3,13 +3,14 @@ class Troubleticket < ApplicationRecord
   after_initialize :set_defaults
 
  #validates_presence_of :user_id
+ 
   validates_presence_of :subject
   validates_presence_of :message
 
 
  def set_defaults
   # self.user_id ||= ?
-   self.status ||= true
+   # self.status ||= true
 end
 
 end
