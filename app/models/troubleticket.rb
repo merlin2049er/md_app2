aea6619d73a91dc1816f04085d9b852b379fa271
Joe Guerra <guerra_joe@hotmail.com>
2020-05-22 12:57:51 -0400

    ..

--- app/models/troubleticket.rb
+++ app/models/troubleticket.rb
@@ -1,16 +1,7 @@
 class Troubleticket < ApplicationRecord
   belongs_to :user
-#  before_create :set_defaults
-
- #validates_presence_of :user_id
 
   validates_presence_of :subject
   validates_presence_of :message
-
-
- #def set_defaults
-  # self.user_id ||= ?
-  #  self.status ||= true
- #end
 
 end
