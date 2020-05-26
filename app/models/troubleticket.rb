0995e429e815dbb1cd6574e79327c14dcd2e708d
Joe Guerra <guerra_joe@hotmail.com>
2020-05-26 10:24:09 -0400

    add ticketnotes to troubleticket show...

--- app/models/troubleticket.rb
+++ app/models/troubleticket.rb
@@ -1,6 +1,7 @@
 class Troubleticket < ApplicationRecord
   belongs_to :user
   has_many   :ticketnotes, dependent: :destroy
+  accepts_nested_attributes_for :ticketnotes
 
   validates_presence_of :subject
   validates_presence_of :message
