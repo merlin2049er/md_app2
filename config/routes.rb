eee29742b3a306d10e70272ebf9cec82c274c9d9
Joe Guerra <guerra_joe@hotmail.com>
2020-05-27 14:01:00 -0400

    ..

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,6 @@ Rails.application.routes.draw do
       resources :feedbacks , only:[:new,:create]
   end
 
-
   scope '/checkout' do
   post 'create', to: 'checkout#create',   as: 'checkout_create'
    get 'cancel', to: 'checkout#cancel',   as: 'checkout_cancel'
