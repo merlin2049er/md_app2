8ab71c9f58eba2d9e11e0f0d0feb29dc2745f415
Joe Guerra <guerra_joe@hotmail.com>
2020-05-30 12:14:18 -0400

    ..

---
+++ app/services/stripe/event_handler.rb
@@ -0,0 +1,40 @@
+# Stripe module
+module Stripe
+ # stripe main class EventHandler
+ class EventHandler
+   def call(event)
+     method = 'handle_' + event.type.tr('.', '_')
+     send method, event
+   rescue JSON::ParserError => e
+     render json: { status: 400, error: 'Invalid payload' }
+     Raven.capture_exception(e)
+   rescue Stripe::SignatureVerificationError => e
+     render json: { status: 400, error: 'Invalid signature' }
+     Raven.capture_exception(e)
+   end
+ end
+
+ def handle_charge_dispute_created(event)
+   # your code goes here
+ end
+
+ def handle_invoice_payment_failed(event)
+   # your code goes here
+ end
+
+ def handle_invoice_payment_succeeded(event)
+   # your code goes here
+ end
+
+end
+
+# method = 'handle_' + event.type.tr('.', '_')
+
+# event.type
+# This will give the name of the event. In our case, it will give ‘charge.dispute.created’ and replace the ‘.’ with ‘_’ so it will become ‘charge_dispute_created’ and concat it with ‘handle_’.
+# so it becomes ‘handle_charge_dispute_created’ as a method.
+
+# send method, event
+
+# This will call the method based on the method variable value. In our case, it will call ‘handle_charge_dispute_created’
+# Same way it will handle the other events based on your events you need to define the method in service put your code in it!
