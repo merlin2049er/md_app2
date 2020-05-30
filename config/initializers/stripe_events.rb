685b4baa570c5fc21d04dd9847202d3b80e68d97
Joe Guerra <guerra_joe@hotmail.com>
2020-05-30 11:47:28 -0400

    ..

--- config/initializers/stripe_events.rb
+++ config/initializers/stripe_events.rb
@@ -1,6 +1,9 @@
 
-StripeEvent.signing_secret = Rails.application.credentials.stripe[Rails.env.to_sym][:signing_secret]
+# this doesn't fucken work...
 
-StripeEvent.configure do |events|
-  events.subscribe 'invoice.', Stripe::InvoiceEventHandler.new
-end
+#Stripe.api_key = Rails.application.credentials.stripe[Rails.env.to_sym][:publishable_key]
+#StripeEvent.signing_secret = Rails.application.credentials.stripe[Rails.env.to_sym][:signing_secret]
+
+#StripeEvent.configure do |events|
+#  events.subscribe 'invoice.', Stripe::InvoiceEventHandler.new
+#end
