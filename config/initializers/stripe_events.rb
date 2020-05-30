461cb70643f919f748e4485ce6fd6f81d7d89a5b
Joe Guerra <guerra_joe@hotmail.com>
2020-05-30 14:41:18 -0400

    ..

--- config/initializers/stripe_events.rb
+++ config/initializers/stripe_events.rb
@@ -1,7 +1,7 @@
 
 Stripe.api_key             = ENV['STRIPE_SECRET_KEY']
 
-#StripeEvent.signing_secret = ENV['STRIPE_SIGNING_SECRET']
+StripeEvent.signing_secret = ENV['STRIPE_SIGNING_SECRET']
 # StripeEvent.signing_secret = Rails.application.credentials.stripe[Rails.env.to_sym][:signing_secret]
 
 StripeEvent.configure do |events|
