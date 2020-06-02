1a4ae02a348386caec26e63f175631ac56696e1c
Joe Guerra <guerra_joe@hotmail.com>
2020-06-02 11:55:31 -0400

    ..

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -99,7 +99,6 @@ class CheckoutController < ApplicationController
       transaction = Transaction.new do |t|
         t.user_id = cart.user_id
         t.postal_carrier = 'Canada Post'
-        t.shipped = False
         t.invoice_number = '123...FIX THIS- HYPERLINK TO PDF'
         t.tracking_number = '...coming soon...'
         t.transaction_msg = "...shipment pending..."
