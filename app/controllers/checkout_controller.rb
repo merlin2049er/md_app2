bb77aff28cf41e4f28b1eea8c8fee31ada60ce4f
Joe Guerra <guerra_joe@hotmail.com>
2020-06-02 16:11:52 -0400

    ..

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -99,7 +99,7 @@ class CheckoutController < ApplicationController
       transaction = Transaction.new do |t|
         t.user_id = cart.user_id
         t.postal_carrier = 'Canada Post'
-        t.invoice_number = '<a href= '<%= @success.payment_intent.charges.data[0].receipt_url %>' >...here...</a>'
+        t.invoice_number = '<a href= '<%= @success.payment_intent.charges.data[0].receipt_url %>' >...here...</a>'.html_safe
         t.tracking_number = '...coming soon...'
         t.transaction_msg = "...shipment pending..."
       end
