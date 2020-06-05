adda2502a924d93b17c8a9b84b4a3f57723da7de
Joe Guerra <guerra_joe@hotmail.com>
2020-06-05 08:42:44 -0400

    ..

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -4,7 +4,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Component from './components/app'
+import Component from './components/App'
 
 
 document.addEventListener('DOMContentLoaded', () => {
