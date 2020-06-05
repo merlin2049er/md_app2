e3b0c87ff08787b144cfeb66168d2194f7d7938b
Joe Guerra <guerra_joe@hotmail.com>
2020-06-05 08:27:31 -0400

    add react

---
+++ app/javascript/components/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+  render(){
+    return(
+      <div>Hello, this is your app speaking!</div>
+    )
+  }
+
+}
+Export default App
