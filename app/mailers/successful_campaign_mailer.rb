145205d9b55bb0d341d33b4c7e8bf4e349ed9334
Joe Guerra <guerra_joe@hotmail.com>
2020-06-11 11:53:10 -0400

    ..

--- app/mailers/successful_campaign_mailer.rb
+++ app/mailers/successful_campaign_mailer.rb
@@ -2,9 +2,11 @@ class SuccessfulCampaignMailer < ApplicationMailer
   default from: 'no-reply@jginfosys.com'
 
    def successful_campaign_email
-     @user = params[:user]
+     #@user = params[:user]
+
      @url  = 'https://desolate-journey-54830.herokuapp.com/'
-     mail(to: @user.email, subject: 'The campaign was a success...')
+    # mail(to: @user.email, subject: 'The campaign was a success...')
+    mail(to: @user, subject: 'The campaign was a success...')
    end
 
 end
