3c17b0e2941f77c6cbcda26fe80f4905b2cedff3
Joe Guerra <guerra_joe@hotmail.com>
2020-06-12 13:25:23 -0400

    ..

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,7 +69,8 @@ class ProductsController < ApplicationController
     user_id.each do |id|
       @user = User.find_by_id(id)
       #SuccessfulCampaignMailer.with(user: @user).successful_campaign_email.deliver_now
-      SuccessfulCampaignMailer.with(user: @user).successful_campaign_email.deliver_now
+      #SuccessfulCampaignMailer.with(user: @user).successful_campaign_email.deliver_now
+      SuccessfulCampaignMailer.successful_campaign_email(@user).deliver_now
     end
 
 end
