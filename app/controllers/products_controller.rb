72c913e8fc4682e7753c03baed59d82c4e5f79cd
Joe Guerra <guerra_joe@hotmail.com>
2020-06-12 12:28:30 -0400

    ..

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -65,13 +65,11 @@ class ProductsController < ApplicationController
     end
 
     # find all users in this successful campaign
-    user_id = Cart.where(product_id: @product.id).distinct.pluck(:user_id)
-
+    user_id = Cart.where(product_id: @product.id).pluck('DISTINCT(user_id)')
     user_id.each do |id|
       @user = User.find_by_id(id)
       #SuccessfulCampaignMailer.with(user: @user).successful_campaign_email.deliver_now
       SuccessfulCampaignMailer.with(user: @user).successful_campaign_email.deliver_now
-
     end
 
 end
