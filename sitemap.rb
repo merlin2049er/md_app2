1257af60fa5388a9937129e3936ce2371a1879d5
Joe Guerra <guerra_joe@hotmail.com>
2020-08-07 12:50:35 -0400

    sitemap

---
+++ sitemap.rb
@@ -0,0 +1,9 @@
+require 'rubygems'
+require 'sitemap_generator'
+
+SitemapGenerator::Sitemap.default_host = 'https://desolate-journey-54830.herokuapp.com/'
+SitemapGenerator::Sitemap.create do
+  add '/home', :changefreq => 'weekly', :priority => 0.9
+  add '/contact_us', :changefreq => 'yearly'
+end
+SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
