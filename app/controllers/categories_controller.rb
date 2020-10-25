bd87fbb21cd38040b367b9bd81bb6764c7de0e52
Joe Guerra <guerra_joe@hotmail.com>
2020-10-25 09:24:13 -0400

    fix date range on category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -36,7 +36,7 @@ class CategoriesController < ApplicationController
     @title = @category.name
     puts @category.products.inspect
 
-    @pagy, @products = pagy(@category.products.where('draft = ? and active = ? and funded = ? and startdate < ? and enddate > ? ', false , true, false , todaydate, todaydate ))
+    @pagy, @products = pagy(@category.products.where('draft = ? and active = ? and funded = ? and startdate <= ? and enddate >= ? ', false , true, false , todaydate, todaydate ))
 
     # @products = @category.products.where( 'enddate > ?', todaydate )
 
