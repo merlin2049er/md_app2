91f8b602215e8e859470ec1813526eb087ae8198
Joe Guerra <merlin2049er@gmail.com>
2020-12-13 13:58:06 -0500

    add regex valid email check to contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class Contact < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
   validates_presence_of :name
-  validates_presence_of :email
+  #validates_presence_of :email
   validates_presence_of :message
 end
