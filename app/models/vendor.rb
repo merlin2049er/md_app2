f156b3ebce29c6b386d746515de7b002fe012fd9
Joe Guerra <merlin2049er@gmail.com>
2021-01-24 13:06:43 -0500

    more validation on vendor form

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -6,8 +6,9 @@ class Vendor < ApplicationRecord
   validates_presence_of :address1
   validates_presence_of :city
   validates_presence_of :state
+  validates_presence_of :country
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
-    validates :email, uniqueness: { case_sensitive: false }
+  validates :email, uniqueness: { case_sensitive: false }
   validates_presence_of :phone
   validates_presence_of :website
   validates_presence_of :product
@@ -16,6 +17,9 @@ class Vendor < ApplicationRecord
   validates_presence_of :locally
   validates_presence_of :payments
 
+  validates_numericality_of :processing, greater_than: 0
+  validates_numericality_of :processing, less_than: 5
 
+  validates_length_of :product, minimum: 35
 
 end
