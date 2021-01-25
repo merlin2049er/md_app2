139ab694e1ff1a727ac15e2e57aa71165ff4a251
Joe Guerra <merlin2049er@gmail.com>
2021-01-25 12:58:03 -0500

    recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,4 @@
-Recaptcha.configuration do |config|
-  config.site_key = ENV[:RECAPTCHA_SITE_KEY]
-  config.secret_key = ENV[:RECAPTCHA_PRIVATE_KEY]
-  # Uncomment the following line if you are using a proxy server:
-  # config.proxy = 'http://myproxy.com.au:8080'
+Recaptcha.configure do |config|
+    config.public_key = ENV["RECAPTCHA_PUBLIC_KEY"]
+    config.private_key = ENV["RECAPTCHA_PRIVATE_KEY"]
 end
