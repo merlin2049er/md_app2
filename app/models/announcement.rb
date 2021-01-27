ad85d7ebe279c966a99ad0501763b612c91610cc
Joe Guerra <merlin2049er@gmail.com>
2021-01-27 11:38:47 -0500

    annoucement bar

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,4 +1,10 @@
 class Announcement < ApplicationRecord
 
     validates_presence_of :announcement
+
+  scope :top_annoucement
+    lambda {
+      where(enabled: true).order('startdate desc').limit(1)
+    }
+
 end
