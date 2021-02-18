073f8476f97d304e05bafdb0de3e426599a77a40
Joe Guerra <merlin2049er@gmail.com>
2021-02-18 12:25:12 -0500

    polling

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,9 +1,14 @@
 class Poll < ApplicationRecord
+  after_initialize :set_defaults
   has_many :poll_options,  dependent: :destroy
 
   validates_presence_of :name
   validates_presence_of :description
 
   accepts_nested_attributes_for :poll_options, reject_if: :all_blank, allow_destroy: true
+
+  def set_defaults
+    self.enabled ||= 'true'
+  end
 
 end
--- app/models/poll_option.rb
+++ app/models/poll_option.rb
@@ -1,6 +1,15 @@
 class PollOption < ApplicationRecord
   #belongs_to :poll
+  after_initialize :set_defaults
   validates_presence_of :poll
   validates_presence_of :poll_url
+
+
+  def set_defaults
+    self.enabled ||= 'true'
+    self.poll_url ||=
+      'https://res.cloudinary.com/dlfodsgbd/image/upload/v1590070578/TippingPoint/photo_not_available.png'
+    self.poll_url_enabled ||= 'true'
+  end
 
 end
