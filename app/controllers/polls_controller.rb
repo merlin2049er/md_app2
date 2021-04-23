b9c0fc593dd774c10795cf403765b22009323920
Joe Guerra <merlin2049er@gmail.com>
2021-04-23 12:06:30 -0400

    fix percentage voting

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -19,7 +19,7 @@ class PollsController < ApplicationController
     add_breadcrumb 'Poll'
 
     #fix this put in right variables...
-    @polloption_sum = Voted.where(polloption_id: 42).count
+    #@polloption_sum = Voted.where(polloption_id: 42).count  #put this in the view...
     @polloption_total = Voted.where(poll_id: @poll.id).count
 
     #binding.pry
