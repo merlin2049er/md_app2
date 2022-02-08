a0729fa134fccff30ff335e8eb629bfa720fb111
Joe Guerra <merlin2049er@gmail.com>
2022-02-08 13:31:31 -0500

    validation for blog content

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,7 +1,17 @@
 class Blog < ApplicationRecord
 
+  # for validation only
+  attr_accessor :content
+
   validates_presence_of :title
   validates_presence_of :description
   has_rich_text :content
-  
+
+
+  validate :post_content_present
+
+  def post_content_present
+    errors.add(:content, "can't be empty") if content.blank?
+  end
+
 end
