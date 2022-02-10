e8c2c496ae3d3db3a56b455c1c8e1d288c7f4668
Joe Guerra <merlin2049er@gmail.com>
2022-02-10 15:53:25 -0500

    ..

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'] , {redirect_uri: 'https://desolate-journey-54830.herokuapp.com/users/auth/google_oauth2/callback'}
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'] , {
+
+      scope: 'userinfo.email, userinfo.profile, https://desolate-journey-54830.herokuapp.com',
+      prompt: 'select_account',
+      image_aspect_ratio: 'square',
+      image_size: 50,
+      redirect_uri: 'https://desolate-journey-54830.herokuapp.com/users/auth/google_oauth2/callback'}
 
 end
 # OmniAuth.config.allowed_request_methods = %i[get]
