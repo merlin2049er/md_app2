77dd270abfdc3c3fd68cc7b8f10c24263cdc0b43
Joe Guerra <merlin2049er@gmail.com>
2022-02-10 15:33:06 -0500

    fix authentication?

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,35 +2,30 @@
 
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController # You need to implement the method below in your model (e.g. app/models/user.rb)
+
+
     def facebook
+      handle_auth "Facebook"
+    end
+
+    def google_oauth2
+      handle_auth "Google"
+    end
+
+
+    def handle_auth(kind)
       @user = User.from_omniauth(request.env['omniauth.auth'])
 
       if @user.persisted?
         sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
         if is_navigational_format?
-          set_flash_message(:notice, :success, kind: 'Facebook')
+          set_flash_message(:notice, :success, kind: kind)
         end
       else
-        session['devise.facebook_data'] = request.env['omniauth.auth']
+        session['devise.auth_data'] = request.env['omniauth.auth']
         redirect_to new_user_registration_url
       end
     end
-
-
-    def google_oauth2
-      # You need to implement the method below in your model (e.g. app/models/user.rb)
-      @user = User.from_omniauth(request.env['omniauth.auth'])
-
-      if @user.persisted?
-        sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
-      if is_navigational_format?
-          set_flash_message(:notice, :success, kind: 'Google')
-        end
-      else
-        session['devise.google_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
-        redirect_to new_user_registration_url
-      end
-  end
 
     def failure
       redirect_to root_path
