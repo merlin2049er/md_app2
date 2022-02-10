d2155315c7c352c93a17e344362b8c0157059c44
Joe Guerra <merlin2049er@gmail.com>
2022-02-10 15:02:29 -0500

    fix google?

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -22,11 +22,13 @@ module Users
       @user = User.from_omniauth(request.env['omniauth.auth'])
 
       if @user.persisted?
-        flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-        sign_in_and_redirect @user, event: :authentication
+        sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
+      if is_navigational_format?
+          set_flash_message(:notice, :success, kind: 'Google')
+
       else
         session['devise.google_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
-        redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+        redirect_to new_user_registration_url
       end
   end
 
